d5a0963c9edf69a3695f028114b458bdaccc2a2e
Saul Manzano <samanzul99@email.arizona.edu>
2019-04-19 20:06:21 -0700

    split initial

---
+++ tracker_service.js
@@ -0,0 +1,57 @@
+/*Author: Saul Manzano & Miranda Hampton
+Filename: tracker_service.js
+Purpose: This file parses data and sends 
+Class: CSC 337 Spring 2019
+*/
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+app.use(express.static('public'));
+
+function read_file(file_name) {
+	var info = ""
+	try {  
+	    info = fs.readFileSync(file_name, 'utf8');
+	       
+	} catch(e) {
+	    console.log('Error:', e.stack);
+	}
+	return info;
+}
+
+function parse_info(info_data){
+	var lines = info_data.split("\n");
+	return lines;
+}
+
+//http://localhost:3000/?mode=initial
+console.log('web service started');
+app.get('/', function (req, res) {
+	
+	res.header("Access-Control-Allow-Origin", "*");
+	let title = req.query.title;
+	let mode = req.query.mode;
+
+	if(mode == undefined) {
+		res.status(400);
+		res.send("Missing required parameters");
+	}
+	
+	
+
+	if (mode == "initial"){
+
+		let file_name =  "roster.txt";
+		let info_data = read_file(file_name);
+		output = parse_info(info_data);
+		console.log(output);
+		//console.log(info_data);
+	}
+	
+
+
+	
+})
+
+app.listen(3000);
